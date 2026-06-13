51954387ad658a0da650bd38c6ec1ff862f31713
Ensar321 <ensarpushkolii12@gmail.com>
2026-06-13 14:27:55 +0200

    Add files via upload

---
+++ moduli 11/main.py
@@ -0,0 +1,67 @@
+import os
+import datetime
+
+file_path = 'example.txt'
+if os.path.exists("example.txt"):
+    print("file exists")
+else:
+    print("not found")
+
+with open("example.txt", "a") as file:
+    file.write("the best group ever")
+    file.close()
+with open("example.txt","r") as file:
+    for line in file:
+        words =line.strip().split()
+        print(words)
+
+
+name = "Alice"
+age = 30
+
+with open("example.txt","w") as file:
+    file.write(f"Name: {name}\n")
+    file.write(f"Age: {age}\n")
+
+
+
+
+current_datetime = datetime.datetime.now()
+print(current_datetime)
+print("Year" , current_datetime.year )
+print("Month" , current_datetime.month )
+print("DAY" , current_datetime.day)
+print("Hour" , current_datetime.hour)
+print("Minute" , current_datetime.minute)
+print("Second" , current_datetime.second )
+print("Microsecond" , current_datetime.microsecond )
+
+
+
+
+
+deadline_time=datetime.time(12,00,00,00,)
+print(deadline_time)
+
+print("Ora",deadline_time.hour)
+print("minuta",deadline_time.minute)
+print("sekonda",deadline_time.second)
+print("mikrosekonda",deadline_time.microsecond)
+
+
+
+
+
+
+
+
+
+duration = datetime.timedelta(days= 5 ,hours = 3)
+print(duration)
+
+
+new_date = current_datetime + duration
+print (new_date)
+
+previous_date = current_datetime - duration
+print (previous_date)
